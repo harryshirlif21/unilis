eac7c38c894106220f875991dc44bb52c87fc320
Harry Shirlif <hillarymwendi21@gmail.com>
2025-11-20 12:05:02 +0300

    your commit message here

--- lecturer/upload_notes.php
+++ lecturer/upload_notes.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-require_once 'config/db.php';
+require_once '../config/db.php';
 
 // --- Authentication ---
 if(!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'lecturer'){
