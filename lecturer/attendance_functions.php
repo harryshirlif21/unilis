3d4b9a6c5a3184481b414df8cebaf5709e7a9dbf
Harry Shirlif <hillarymwendi21@gmail.com>
2025-11-28 08:47:49 +0300

    update

--- lecturer/attendance_functions.php
+++ lecturer/attendance_functions.php
@@ -1,7 +1,8 @@
 <?php
 // attendance_functions.php — WORKING VERSION FOR XAMPP
 require_once '../config/db.php';
-require_once '../includes/mailer.php'; // use your working mailer
+//require_once '../includes/mailer.php'; // use your working mailer
+require_once __DIR__ . '/../includes/mailer.php';
 
 // ========================
 // SEND ATTENDANCE EMAIL
