caa13c27f45bb198dfa3fa72dca4b43feea208d3
Harry Shirlif <hillarymwendi21@gmail.com>
2025-12-01 08:48:10 +0300

    update

--- student/student_auto_mark.php
+++ student/student_auto_mark.php
@@ -1,6 +1,9 @@
 <?php
 ob_start();
-session_start();
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 
 require_once '../config/db.php';
 require_once '../lecturer/attendance_functions.php';
