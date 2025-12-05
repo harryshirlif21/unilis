478b63b3c9ed832b55c6dcab9f6095917aad33a0
Harry Shirlif <hillarymwendi21@gmail.com>
2025-12-05 10:35:50 +0300

    update

--- student/take_assignment.php
+++ student/take_assignment.php
@@ -4,7 +4,7 @@ session_start();
 
 // Redirect if not logged in or not a student
 if (!isset($_SESSION['user_id']) || $_SESSION['user_role'] !== 'student') {
-    header("Location: ../index.php");
+    header("Location: ../index.html");
     exit;
 }
 
