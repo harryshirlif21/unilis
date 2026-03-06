54476267f1e863fad253e7d39c18129eca77b19a
Harry Shirlif <hillarymwendi21@gmail.com>
2026-03-06 10:27:03 +0300

    update

--- test_email.php
+++ test_email.php
@@ -8,22 +8,18 @@ $mail = new PHPMailer(true);
 
 try {
 
-    // SMTP SERVER SETTINGS
     $mail->isSMTP();
-    $mail->Host       = 'unilis.jhubafrica.com';
+    $mail->Host       = 'mail.unilis.jhubafrica.com';
     $mail->SMTPAuth   = true;
     $mail->Username   = 'noreply@unilis.jhubafrica.com';
-    $mail->Password   = 'Man.18hattan';   // your email password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS; // SSL
-    $mail->Port       = 465;
+    $mail->Password   = 'Man.18hattan';
 
-    // SENDER
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+    $mail->Port       = 587;
+
     $mail->setFrom('noreply@unilis.jhubafrica.com', 'UNILIS');
-
-    // RECIPIENT
     $mail->addAddress('mwendihillary21@gmail.com');
 
-    // EMAIL CONTENT
     $mail->isHTML(true);
     $mail->Subject = 'UNILIS Email Test';
     $mail->Body    = '<h2>UNILIS Mail Server Working ✅</h2>';
@@ -35,5 +31,4 @@ try {
 } catch (Exception $e) {
 
     echo "❌ Email failed: {$mail->ErrorInfo}";
-
 }
