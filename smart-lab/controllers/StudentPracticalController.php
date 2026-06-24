2236d5825aec72cd882dd4f3d2117c49228e4021
Harry Shirlif <hillarymwendi21@gmail.com>
2026-06-24 07:31:39 +0300

    update

--- smart-lab/controllers/StudentPracticalController.php
+++ smart-lab/controllers/StudentPracticalController.php
@@ -7,7 +7,6 @@ if ($is_production) {
     require_once __DIR__.'/../config/database_production.php';
 } else {
     require_once __DIR__.'/../config/app.php';
-    require_once __DIR__.'/../config/database.php';
 }
 
 require_once __DIR__.'/../auth/Auth.php';
